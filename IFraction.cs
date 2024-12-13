27863f7c92ae4c8a22a560b82b5e2be2664d49f2
nenananana <160021396+nenananana@users.noreply.github.com>
2024-12-13 13:34:59 +0500

    Add files via upload

---
+++ IFraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_lab6
+{
+    public interface IFraction
+    {
+        double ToDouble(); 
+        void SetNewValues(int numerator, int denominator); 
+    }
+   
+}
---
+++ MeowManager.cs
@@ -0,0 +1,20 @@
+﻿using sharp_lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_lab6
+{
+    public class MeowManager
+    {
+        public static void MakeAllMeow(IEnumerable<IMeowable> meowables)
+        {
+            foreach (var meowable in meowables)
+            {
+                meowable.Meow();
+            }
+        }
+    }
+}
